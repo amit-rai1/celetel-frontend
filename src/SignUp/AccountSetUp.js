a2976db231382998601a5113c17eb0a09d2fbad5
ManishKuPatra254 <manishpatra254@gmail.com>
2023-12-13 16:14:21 +0530

    added the toaster and password see feature

--- src/SignUp/AccountSetUp.js
+++ src/SignUp/AccountSetUp.js
@@ -3,11 +3,33 @@ import { Fragment, useState, useEffect } from 'react'
 import './AccountSetUp.css'
 import { Link, useNavigate } from 'react-router-dom';
 import { accountSetup, getClientById } from '../Service/auth.service';
+import { RxEyeClosed } from "react-icons/rx";
+import { TfiEye } from "react-icons/tfi";
+import toast, { Toaster } from 'react-hot-toast';
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+export function CircularIndeterminate() {
+    return (
+        <Box sx={{
+            display: "flex",
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+            width: '100%'
+        }}>
+            <CircularProgress sx={{ color: "#FDBA46" }} size={70} />
+        </Box>
+    );
+}
 
 export function AccountSetUp() {
     const navigateToLogin = useNavigate();
 
+    const [loading, setLoading] = useState(false);
     const [email, setEmail] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const [formData, setFormData] = useState({
         password: '',
         confirmPassword: '',
@@ -21,7 +43,7 @@ export function AccountSetUp() {
         const getClientData = async () => {
             console.log(clientId, "clientId");
             try {
-                const response = await getClientById(clientId); 
+                const response = await getClientById(clientId);
                 const { result } = response;
 
                 if (result) {
@@ -38,13 +60,21 @@ export function AccountSetUp() {
 
 
 
-
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData((prevFormData) => ({
             ...prevFormData,
             [name]: value,
         }));
+    };
+
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+
+    const toggleConfirmPasswordVisibility = () => {
+        setShowConfirmPassword((prevShowPassword) => !prevShowPassword);
     };
 
 
@@ -55,10 +85,25 @@ export function AccountSetUp() {
         const clientId = localStorage.getItem('clientId');
 
         try {
+
+            if (!formData.password || !formData.confirmPassword) {
+                console.error('Password or confirmPassword is empty');
+                toast.error('Password or confirm password is empty', { duration: 2000 });
+                return;
+            }
+
+
             if (formData.password !== formData.confirmPassword) {
                 // Password and ConfirmPassword do not match
                 console.error('Passwords do not match');
+                toast.error('Passwords do not match', { duration: 2000 });
                 return;
+            }
+            else {
+                setLoading(true);
+                toast.success('Password matches', { duration: 2000 });
+                // alert('Success')
+
             }
 
             const response = await accountSetup(clientId, {
@@ -67,7 +112,7 @@ export function AccountSetUp() {
 
             console.log('Password updated successfully:', response);
             navigateToLogin('/login');
-        } 
+        }
         catch (error) {
             console.error('Error updating password:', error.message);
         }
@@ -79,48 +124,82 @@ export function AccountSetUp() {
     };
     return (
         <Fragment>
-            <div className="main_acc_set_up">
-                <div className="app_container">
-                    <form onSubmit={handleSubmit} className="form_container">
-                        <h1>Account set up</h1>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            disabled
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={email}
-                            onChange={handleInputChange}
-                            required placeholder='Enter here'
-                        />
+            {
+                loading ? <CircularIndeterminate /> : (
+                    <div className="main_acc_set_up">
+                        <div className="app_container">
+                            <form onSubmit={handleSubmit} className="form_container">
+                                <h1>Account set up</h1>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    className='input_1_email'
+                                    disabled
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={handleInputChange}
+                                    required placeholder='Enter here'
+                                />
 
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleInputChange}
-                            required placeholder='Enter here'
-                        />
+                                <label htmlFor="password">Password</label>
 
-                        <label htmlFor="confirmPassword">Confirm Password</label>
-                        <input
-                            type="password"
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            value={formData.confirmPassword}
-                            onChange={handleInputChange}
-                            required
-                            placeholder='Enter here'
-                        />
-                        <div className="submit_form">
-                            <button type="submit" onClick={handleUpdate}>Create</button>
-                            <p>Already have an account? <Link to={'/login'}>Log in</Link></p>
+                                <div className="new_pass">
+                                    <input
+                                        className='input_password'
+                                        type={showPassword ? 'text' : 'password'}
+                                        id="password"
+                                        name="password"
+                                        value={formData.password}
+                                        onChange={handleInputChange}
+                                        required placeholder='Enter here'
+
+                                    />
+                                    <button
+                                        type="button"
+                                        className="password_toggle_btn"
+                                        onClick={togglePasswordVisibility}
+                                    >
+                                        {showPassword ? <TfiEye /> : <RxEyeClosed />}
+                                    </button>
+
+                                </div>
+
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+
+                                <div className="input_con_pass">
+                                    <input
+                                        className='con_pass_input'
+                                        type={showConfirmPassword ? 'text' : 'password'}
+                                        id="confirmPassword"
+                                        name="confirmPassword"
+                                        value={formData.confirmPassword}
+                                        onChange={handleInputChange}
+                                        required
+                                        placeholder='Enter here'
+                                    />
+
+                                    <button
+                                        type="button"
+                                        className="con_password_toggle_btn"
+                                        onClick={toggleConfirmPasswordVisibility}
+                                    >
+                                        {showConfirmPassword ? <TfiEye /> : <RxEyeClosed />}
+                                    </button>
+
+                                </div>
+                                <div className="submit_form">
+                                    <button type="submit" onClick={handleUpdate}>Create</button>
+                                    <Toaster />
+                                    <p>Already have an account? <Link to={'/login'}>Log in</Link></p>
+                                </div>
+                            </form>
                         </div>
-                    </form>
-                </div>
-            </div>
+                    </div>
+
+
+                )}
+
         </Fragment>
     )
 }
