847b71d22b8945c7b077fd08f3eaf1b704cb1182
ManishKuPatra254 <manishpatra254@gmail.com>
2024-01-30 11:35:39 +0530

    added new nav

--- src/MainVoicePage/MainVoice.js
+++ src/MainVoicePage/MainVoice.js
@@ -1,7 +1,8 @@
 import './MainVoice.css'
 import React, { Fragment } from 'react'
-import image1 from '../Assets/Untitled design.gif'
-import image2 from '../Assets/communication _ voice, message, memo, record, microphone, conversation 1.png'
+import image1 from '../Assets/GirlVoice.svg'
+import image5 from '../Assets/OOHKAY.svg'
+import image2 from '../Assets/communication _ voice, message, memo, record, microphone, conversation 1.svg'
 import { BiMessageDetail } from "react-icons/bi";
 import { SlGraph } from "react-icons/sl";
 import { LuUsers } from "react-icons/lu";
@@ -9,7 +10,7 @@ import { FaPencil } from "react-icons/fa6";
 import { PiDropboxLogo } from "react-icons/pi";
 import { IoIosSettings } from "react-icons/io";
 import image3 from '../Assets/OBJECT (1).png'
-import image4 from '../Assets/New6.jpg'
+import image4 from '../Assets/Group 1000001797.svg'
 import { Footer } from '../Footer/Footer'
 
 
@@ -17,7 +18,12 @@ export function MainVoice() {
     return (
         <Fragment>
             <div className="main_sol_page10">
-                <h1>A complete voice solution from a single provider</h1>
+                <div className="new_svg_imp_voice">
+                    <h1>A complete voice solution from a single provider</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="149" height="250" viewBox="0 0 149 250" fill="none">
+                        <path d="M161.102 1.30099C182.417 35.0728 193.905 76.4117 158.712 127.497C139.921 154.774 100.177 181.043 73.5403 166.58C55.3989 156.73 40.3229 130.542 66.9465 106.287C96.3464 79.5028 87.6042 140.768 84.8867 147.801C72.1678 180.719 50.02 208.914 19.7692 240.693C8.43741 252.597 18.3017 248.275 28.4118 241.699C37.1839 235.994 47.0473 229.391 54.3601 222.762C56.5905 220.741 39.9239 232.459 37.3685 234.365C26.4103 242.542 13.4829 251.793 7.37309 243.322C6.39161 241.961 -0.643535 228.101 2.77271 225.694C4.55024 224.441 17.9258 228.25 20.1543 228.374C34.6281 229.178 44.8447 226.426 59.9218 217.211" stroke="black" stroke-width="2" stroke-linecap="round" />
+                    </svg>
+                </div>
                 <p>Implementing IVR solutions is indispensable for enterprises
                     aspiring to augment their customer service capabilities.
                     These sophisticated systems enable streamlined and
@@ -30,6 +36,7 @@ export function MainVoice() {
             </div>
             <div className="main_sms1">
                 <div className="sub_sms1">
+                    <img src={image5} alt="" />
                     <img src={image1} alt="" />
                 </div>
             </div>
